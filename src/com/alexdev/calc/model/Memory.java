5ad9e08225ca4d24ac8936d047ec65d7e212c985
ItsmeAlexDev <77814458+ItsmeAlexDev@users.noreply.github.com>
2023-09-22 22:39:57 -0300

    Update memory
    There are still some minor bugs. I shall fix them in the future.

--- src/com/alexdev/calc/model/Memory.java
+++ src/com/alexdev/calc/model/Memory.java
@@ -1,5 +1,9 @@
 package com.alexdev.calc.model;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.Double.toString;
+import static java.lang.Integer.parseInt;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +17,10 @@ public class Memory {
 	
 	private final List<MemoryObserver> observers = new ArrayList<>();
 	
+	private CommandType operation = null;
+	private boolean takePlace = false;
 	private String currentValue = "";
+	private String bufferValue = "";
 	
 	private Memory() {}
 	
@@ -22,6 +29,8 @@ public class Memory {
 	}
 	
 	public String getCurrentValue() {
+		currentValue = currentValue.replace(".", ",");
+		if(currentValue.startsWith(",")) currentValue = currentValue.replace(",", "0,");
 		return currentValue.isEmpty() ? "0" : currentValue;
 	}
 	
@@ -32,12 +41,61 @@ public class Memory {
 	public void doCommand(String value) {
 		CommandType commandType = detectCommandType(value);
 		
-		if(commandType == CommandType.CLEAR) currentValue = "";
-		else currentValue += value;
+		if(commandType == null) return;
+		else if(commandType == CommandType.CLEAR) {
+			currentValue = "";
+			bufferValue = "";
+			takePlace = false;
+			operation = null;
+		}
+		else if(commandType == CommandType.NUMBER ||
+		   commandType == CommandType.COMMA) {
+			currentValue = takePlace ? value : currentValue + value;
+			takePlace = false;
+		}
+		else if (commandType == CommandType.INVERSE) {
+			Double currentNumber = parseDouble(getCurrentValue().replace(",", ".")) * -1;
+			if(currentNumber == 0) return;
+			String result = Double.toString(currentNumber);
+			boolean isInteger = result.endsWith(".0");
+			currentValue = isInteger ? result.replace(".0", "") : result;
+		}
+		else {
+			takePlace = true;
+			currentValue = operationResult();
+			bufferValue = currentValue;
+			operation = commandType;
+		}
 		
 		observers.forEach(obs -> obs.changedValue(getCurrentValue()));
 	}
 	
+	private String operationResult() {
+		if(operation == null) return currentValue;
+		
+		double bufferValue = parseDouble(this.bufferValue.replace(",", "."));
+		double currentValue = parseDouble(this.currentValue.replace(",", "."));
+		double total = 0;
+		
+		switch (operation) {
+			case ADDITION:
+				total = bufferValue + currentValue;
+				break;
+			case SUBTRACTION:
+				total = bufferValue - currentValue;
+				break;
+			case MULTIPLICATION:
+				total = bufferValue * currentValue;
+				break;
+			case DIVISION:
+				total = bufferValue / currentValue;
+				break;
+		}
+		String totalString = Double.toString(total);
+		boolean isInteger = totalString.endsWith(".0");
+		return isInteger ? totalString.replace(".0", "") : totalString;
+	}
+
 	private CommandType detectCommandType(String value) {
 		
 		if(currentValue.isEmpty() && value == "0") return null;
@@ -53,7 +111,7 @@ public class Memory {
 			if("+".equals(value)) return CommandType.ADDITION;
 			if("-".equals(value)) return CommandType.SUBTRACTION;
 			if("=".equals(value)) return CommandType.EQUALS;
-			if(",".equals(value)) return CommandType.COMMA;
+			if(",".equals(value) && !currentValue.contains(",")) return CommandType.COMMA;
 		}
 		return null;
 	}
